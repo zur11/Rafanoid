7fe97b2a29d963aa1265e80a94247f5b9d65b314
zur11 <cibka11@gmail.com>
2022-04-02 22:54:25 -0500

    2 basic world scene done - ready to work on player.gd

--- player/player.gd
+++ player/player.gd
@@ -5,14 +5,6 @@ const LEFT = -1
 
 export var speed = 4
 
-""" Mueve un solo speed cada presion de tecla
-func _input(event):
-	if event.is_action_pressed("mover_derecha"):
-		position.x += speed
-	if event.is_action_pressed("mover_izquierda"):
-		position.x -= speed
-"""
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
